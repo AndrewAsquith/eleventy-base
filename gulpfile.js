753ff6c7cb41b39f40bea6c5645adfc7c7aac34b
Andrew Asquith <contact@andrewasquith.com>
2019-02-09 22:57:13 -0500

    added gulp-imagemin and corresponding task

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,6 @@
 const gulp = require("gulp");
 require('require-dir')('./_gulp');
 
-gulp.task('build',  gulp.parallel('generate','styles','scripts'));
+gulp.task('build',  gulp.parallel('generate','styles','scripts','images'));
 
 
