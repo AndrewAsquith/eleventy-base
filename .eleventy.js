e9cc2c25d7e76bd558859c8d1f18bdcf8c5f0ba5
Andrew Asquith <contact@andrewasquith.com>
2019-02-09 22:57:49 -0500

    added metadata file, sitemap and modularized 11ty filters and transforms

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,4 @@
-const htmlmin = require('html-minifier');
+
 require('dotenv').config();
 const  env = process.env.ELEVENTY_ENV;
 
@@ -8,18 +8,12 @@ module.exports = function(config) {
     //layout aliases
     config.addLayoutAlias('base', 'layouts/base.njk')
 
-    // Minify HTML Output
-    config.addTransform('htmlmin', function(content, outputPath) {
-        if (env === 'production' && outputPath.endsWith('.html')) {
-            return htmlmin.minify(content, {
-                useShortDoctype: true,
-                removeComments: true,
-                collapseWhitespace: true,
-                minifyJS: true
-            })
-        }
-        return content
-    })
+    //filters
+    config.addFilter('friendlydate', require('./_11ty/filters/friendly-date.js'));
+    config.addFilter('isoDate', require('./_11ty/filters/iso-date.js'));
+
+    //Minify HTML (when ELEVENTY_ENV is production)
+    config.addTransform('htmlmin', require('./_11ty/transforms/html-minify.js'));
 
     // Base Config
     return {
